b020e2149f2e51226bbb4d4af1a0a13d6f27280a
pranaysj <pranaysj3@gmail.com>
2025-09-03 13:08:35 +0530

    Create a Bullet Pool
    Create a Bullet Pool script
    Initialize a collection (Pooled Bullets) to hold the bullet objects.
    Each PooledBullet should have an isUsed variable & a BulletController

---
+++ Assets/Scripts/Bullets/BulletPool.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmicCuration.Bullets
+{
+    public class BulletPool
+    {
+        private BulletView bulletView;
+        private BulletScriptableObject bulletScriptableObject;
+        private List<PooledBullet> pooledBullets = new List<PooledBullet>();
+
+        public BulletPool(BulletView bulletView, BulletScriptableObject bulletScriptableObject)
+        {
+            this.bulletView = bulletView;
+            this.bulletScriptableObject = bulletScriptableObject;
+        }
+
+        private class PooledBullet
+        {
+            public BulletController bulletController;
+            public bool InUsed;
+        }
+
+    }
+}
